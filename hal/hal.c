fe20398328ddb7460c63c9accefc667d6fa8c490
patrick <patrick7c3@gmail.com>
2017-02-04 02:40:24 +0100

    added comments about registers in hal

--- hal/hal.c
+++ hal/hal.c
@@ -82,47 +82,43 @@ void gpio_enableInterrupt(GPIOA_Type *gpio, uint8_t pin, Callback_t callback) {
 
 /* SPI - enable SSI3 */
 void spi_init(void) {
-	SYSCTL->RCGCSSI = (1<<3);
+	SYSCTL->RCGCSSI = (1<<3); // Enable SSI3 module
 	gpio_enable(PORTD);
 	gpio_alternateFunctions(GPIOD_AHB, 0x0D);
 	gpio_digitalEnable(GPIOD_AHB, 0x0D);
 	gpio_direction(GPIOD_AHB, 0x02);
-	GPIOD_AHB->PCTL |= (1<<0) | (1<<8) | (1<<12);
-
-	SSI3->CR1 &= ~(1<<1);
-	SSI3->CR1 = 0x00000000;
-
-	SSI3->CC = 0x00;
-	SSI3->CPSR = 10;
-
-	SSI3->CR0 = (0x7<<0);
-
-	SSI3->CR1 |= (1<<1);
+	GPIOD_AHB->PCTL |= (1<<0) | (1<<8) | (1<<12); //SSI3 PortMux for PD0(SCK), PD2(SDI), PD3(SDO)
+	SSI3->CR1 &= ~(1<<1);  // disable SPI - clear SSE bit
+	SSI3->CR1 = 0x00000000; //Select SSI3 as master
+	SSI3->CC = 0x00; // Use system clk as clk source
+	SSI3->CPSR = 10; // Clk prescaler
+	SSI3->CR0 = (0x7<<0); // 8 data bits, Freescale SPI mode (CPOL0, CPH0)
+	SSI3->CR1 |= (1<<1); // enable SPI - set SSE bit
 }
 
 void writeSPI(uint8_t data) {
 	SSI3->DR = data;
-	while( (SSI3->SR & (1<<0))==0);
+	while( (SSI3->SR & (1<<0))==0); // wait until fifo is empty
 }
 
-/* TIMER */
+/* TIMER - Periodic timer module */
 void timer_init(void) {
-	SYSCTL->RCGCTIMER |= (1<<0);
-	TIMER0->CTL &= ~(1<<0);
-	TIMER0->CFG = 0x00000000;
-	TIMER0->TAMR |= (0x2<<0);
-	TIMER0->TAMR &= ~(1<<4);
-	TIMER0->TAILR = 1600;
-	TIMER0->IMR |= (1<<0);
+	SYSCTL->RCGCTIMER |= (1<<0); // Enable GPTM 0
+	TIMER0->CTL &= ~(1<<0); // Disable timer (clear Timer A Enable)
+	TIMER0->CFG = 0x00000000; // 16/32 bit timer
+	TIMER0->TAMR |= (0x2<<0); // Mode register - 0x2 for periodic mode
+	TIMER0->TAMR &= ~(1<<4); // Count direction - set to 0 for down counter
+	TIMER0->TAILR = 1600; // start value in interrupt load register
+	TIMER0->IMR |= (1<<0); // enable Timer A time-out interrupt mask
 	//NVIC->ISER[0] |= (1<<19);
-	NVIC_EnableIRQ(TIMER0A_IRQn);
-	TIMER0->CTL |= (1<<0);
+	NVIC_EnableIRQ(TIMER0A_IRQn); // NVIC interrupt set enable
+	TIMER0->CTL |= (1<<0); // Enable timer
 }
 
 static uint8_t timer_100us=0;
 
 void TIMER0A_Handler(void) {
-	TIMER0->ICR |= (1<<0);
+	TIMER0->ICR |= (1<<0);  // clear interrupt
 	timer_100us++;
 }
 
@@ -134,7 +130,7 @@ void hal_init(void) {
 
 void hal_process(void) {
 	if(timer_100us) {
-		TIMER0->IMR &= ~(1<<0);
+		TIMER0->IMR &= ~(1<<0); // make timer_tick interrupt-safe
 		timer_tick(timers, timer_100us*100);
 		timer_100us=0;
 		TIMER0->IMR |= (1<<0);
